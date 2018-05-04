d17d59a9b75db40cb22fbac8be23159e981a0892
Jack Vaught <johnmvaught@gmail.com>
2018-05-04 11:13:04 -0700

    [photos] native photo picker works

--- js/components/drawer/Photos.js
+++ js/components/drawer/Photos.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-import { Text, Button, View, Image, TouchableOpacity } from 'react-native';
+import { 
+  Text, 
+  Button, 
+  View, 
+  Image, 
+  TouchableOpacity, 
+  StyleSheet, 
+  PixelRatio 
+} from 'react-native';
 
 import X from './../globals/buttons/x';
-// import ImagePicker from 'react-native-image-crop-picker';
+import ImagePicker from 'react-native-image-picker';
 
 
 class Photos extends Component {
   constructor() {
     super();
+
+    this.state = {
+      avatarSource: null,
+    }
   }
 
   static navigationOptions = ({navigation}) => {
@@ -23,25 +35,74 @@ class Photos extends Component {
     }
   };
 
-  // openImagePicker = async () => {
-  //   try {
-  //     let image = await ImagePicker.openPicker({
-  //       width: 400,
-  //       height: 400,
-  //       cropping: true
-  //     })
-  //     console.log(image)
-  //   } catch (error) {
-  //     console.error(error)
-  //   }
-  // }
+  selectPhotoTapped() {
+    const options = {
+      quality: 1.0,
+      maxWidth: 500,
+      maxHeight: 500,
+      storageOptions: {
+        skipBackup: true
+      }
+    };
+
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled photo picker');
+      }
+      else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      }
+      else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      }
+      else {
+        let source = { uri: response.uri };
+
+        // You can also display the image using data:
+        // let source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+        this.setState({
+          avatarSource: source
+        });
+      }
+    });
+  }
 
   render() {
     return (
-      <View>
+      <View style={styles.container}>
+      <TouchableOpacity onPress={this.selectPhotoTapped.bind(this)}>
+        <View style={[styles.avatar, styles.avatarContainer, {marginBottom: 20}]}>
+        { this.state.avatarSource === null ? <Text>Select a Photo</Text> :
+          <Image style={styles.avatar} source={this.state.avatarSource} />
+        }
+        </View>
+      </TouchableOpacity>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  avatarContainer: {
+    borderColor: '#9B9B9B',
+    borderWidth: 1 / PixelRatio.get(),
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  avatar: {
+    borderRadius: 75,
+    width: 150,
+    height: 150
+  }
+});
 
 export default Photos;
--- js/redux/index.js
+++ js/redux/index.js
@@ -1,12 +1,14 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-// import { composeWithDevTools } from 'redux-devtools-extension';
 // import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
 
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunk)
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 export default store;
